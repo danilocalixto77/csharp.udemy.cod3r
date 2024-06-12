c2f6df08e405e73e5dee90a60f5d08c459da3155
danilocalixto77 <danilocalixto@gmail.com>
2024-06-12 16:55:54 -0400

    study : 202406121655

--- CursoCSharp/CursoCSharp/Excecoes/ExcecoesPersonalizadas.cs
+++ CursoCSharp/CursoCSharp/Excecoes/ExcecoesPersonalizadas.cs
@@ -5,40 +5,45 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace CursoCSharp.Excecoes {
+
+    public class NegativoException : Exception {
+        public NegativoException() { }
+
+        public NegativoException(string message) : base(message) { }
+
+        public NegativoException(string message, Exception inner) : base(message, inner) { }
+    }
+
+    public class ImparException : Exception {
+        public ImparException(string message) : base(message) { }
+
+    }
+
+
     class ExcecoesPersonalizadas {
 
-        public class NegativoException : Exception {
-            public NegativoException() { }
+        public static int PositivoPar() {
+            Random random = new Random();
+            int valor = random.Next(-30, 30);
 
-            public NegativoException(string message) : base(message) { }
-
-            public NegativoException(string message, Exception inner) : base(message, inner) { }    
-        }
-
-        class ExcecoesPersonalizadas {
-            public static int PositivoPar() {
-                Random random = new Random();
-                int valor = random.Next(-30, 30);
-
-                if (valor < 0) {
-                    throw new NegativoException("Numero negativo...");
-                }
-
-                if (valor % 2 == 1) {
-                    throw new ImparException("Valor impar...");
-                }
-                return valor; 
+            if (valor < 0) {
+                throw new NegativoException("Numero negativo...");
             }
 
-            public static void Executar() {
-
+            if (valor % 2 == 1) {
+                throw new ImparException("Valor impar...");
             }
-
+            return valor;
         }
 
-        public class ImparException : Exception { 
-            public ImparException(string message) : base(message) { }
-        
+        public static void Executar() {
+            try {
+                Console.WriteLine(PositivoPar());
+            } catch(NegativoException ex) {
+                Console.WriteLine(ex.Message);
+            } catch(ImparException ex) {
+                Console.WriteLine(ex.Message);
+            }
         }
        
     }
